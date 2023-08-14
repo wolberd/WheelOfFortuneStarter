04c43b1a568103c87f8cd4fd42f433f30e3e0acf
David Wolber <wolber@gmail.com>
2023-08-14 14:40:46 -0700

    Initial commit

---
+++ src/WheelOfFortuneMain.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class WheelOfFortuneMain {
+    public static void main(String[] args) {
+
+        WheelOfFortuneMain wheelOfFortune = new WheelOfFortuneMain();
+
+        List<String> phraseList=null;
+        // Get the phrase from a file of phrases
+        try {
+            phraseList = Files.readAllLines(Paths.get("phrases.txt"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // Get a random phrase from the list
+        Random rand = new Random();
+        int r= rand.nextInt(3);
+        String phrase = phraseList.get(r);
+        System.out.println(phrase);
+
+    }
+
+}
